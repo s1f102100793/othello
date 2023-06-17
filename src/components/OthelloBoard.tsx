89439b88214dbcfce98869e6042d1ac9f79a0cfe
s1f102100793 <85666759+s1f102100793@users.noreply.github.com>
2023-06-17 22:17:22 +0900

    ほぼ完成

--- src/components/OthelloBoard.tsx
+++ src/components/OthelloBoard.tsx
@@ -1,28 +1,20 @@
-import React, { useState, useEffect } from 'react'; 
-import './OthelloBoard.css';
-
+import React, { useState, useEffect } from "react";
+import "./OthelloBoard.css";
 enum CellState {
   EMPTY,
   BLACK,
-  WHITE
+  WHITE,
 }
-
-const initialBoard = Array(8).fill(null).map(() => 
-  Array<CellState>(8).fill(CellState.EMPTY)
-);
-
-// const [highlightedBoard, setHighlightedBoard] = useState(initialBoard.map(row => row.map(() => false)));
-
-// 初期状態で中央に4つの石を置く
+const initialBoard = Array(8)
+  .fill(null)
+  .map(() => Array<CellState>(8).fill(CellState.EMPTY));
 initialBoard[3][3] = CellState.BLACK;
 initialBoard[4][4] = CellState.BLACK;
 initialBoard[3][4] = CellState.WHITE;
 initialBoard[4][3] = CellState.WHITE;
-
 const getStoneCounts = (board: CellState[][]) => {
   let blackCount = 0;
   let whiteCount = 0;
-
   for (let y = 0; y < 8; y++) {
     for (let x = 0; x < 8; x++) {
       if (board[y][x] === CellState.BLACK) {
@@ -32,22 +24,20 @@ const getStoneCounts = (board: CellState[][]) => {
       }
     }
   }
-
   return { blackCount, whiteCount };
 };
-
 const OthelloBoard = () => {
   const [board, setBoard] = useState(initialBoard);
-  const [turn, setTurn] = useState(CellState.BLACK); // 追加
+  const [turn, setTurn] = useState(CellState.BLACK);
   const [isGameOver, setIsGameOver] = useState(false);
-
-  const [highlightedBoard, setHighlightedBoard] = useState(initialBoard.map(row => row.map(() => false)));
-
+  const [
+    highlightedBoard,
+    setHighlightedBoard,
+  ] = useState(initialBoard.map((row) => row.map(() => false)));
   useEffect(() => {
     if (isGameOver) {
       const { blackCount, whiteCount } = getStoneCounts(board);
       let message = "ゲーム終了：";
-
       if (blackCount > whiteCount) {
         message += "黒の勝ち";
       } else if (whiteCount > blackCount) {
@@ -55,69 +45,61 @@ const OthelloBoard = () => {
       } else {
         message += "引き分け";
       }
-
       alert(message);
     }
   }, [isGameOver, board]);
-
   useEffect(() => {
-    updateHighlightedBoard(); // 追加
+    updateHighlightedBoard();
   }, [turn]);
-
+  const resetGame = () => {
+    const resetBoard = initialBoard.map(row => row.map(cell => cell));
+    setBoard(resetBoard);
+    setTurn(CellState.BLACK);
+    setIsGameOver(false);
+  };
   const updateHighlightedBoard = () => {
-    const newHighlightedBoard = initialBoard.map(row => row.map(() => false));
-
+    const newHighlightedBoard = initialBoard.map((row) =>
+      row.map(() => false)
+    );
     for (let y = 0; y < 8; y++) {
       for (let x = 0; x < 8; x++) {
-        if (canPlaceStone(x, y, turn)) {
+        if (canPlaceStone(board, x, y, turn)) {
           newHighlightedBoard[y][x] = true;
         }
       }
     }
-
     setHighlightedBoard(newHighlightedBoard);
   };
-
   const hasLegalMove = (turn: CellState) => {
     for (let y = 0; y < 8; y++) {
       for (let x = 0; x < 8; x++) {
-        if (canPlaceStone(x, y, turn)) {
+        if (canPlaceStone(board, x, y, turn)) {
           return true;
         }
       }
     }
     return false;
   };
-
-
   const handleCellClick = (x: number, y: number) => {
-    // 石を置くことができるか判定する
-    const canPlace = canPlaceStone(x, y, turn);
-  
+    const canPlace = canPlaceStone(board, x, y, turn);
     if (canPlace) {
-      // 新しい状態を生成してsetBoardで更新する
       const newBoard = [...board];
-      newBoard[y][x] = turn; // 現在のターンの色の石を配置する
-  
-      // TODO: 石を挟んでひっくり返す処理を追加する
-      // 石を挟んでひっくり返す処理
+      newBoard[y][x] = turn;
       const directions: [number, number][] = [
-        [0, 1], // 右
-        [0, -1], // 左
-        [1, 0], // 下
-        [-1, 0], // 上
-        [1, 1], // 右下
-        [-1, -1], // 左上
-        [1, -1], // 左下
-        [-1, 1], // 右上
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+        [1, 1],
+        [-1, -1],
+        [1, -1],
+        [-1, 1],
       ];
-
       for (const [dx, dy] of directions) {
         let nx = x + dx;
         let ny = y + dy;
         let hasOpponentPiece = false;
         const piecesToFlip: [number, number][] = [];
-
         while (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
           if (board[ny][nx] === CellState.EMPTY) {
             break;
@@ -137,91 +119,24 @@ const OthelloBoard = () => {
           ny += dy;
         }
       }
-
       setBoard(newBoard);
-  
-      // 次のターンに進む
       const nextTurn = turn === CellState.BLACK ? CellState.WHITE : CellState.BLACK;
       if (hasLegalMove(nextTurn)) {
         setTurn(nextTurn);
       } else {
-        // 次のターンもパスされる場合
         if (hasLegalMove(turn)) {
-          alert(nextTurn === CellState.BLACK ? "黒はパスです" : "白はパスです");  // アラートを表示
-          // ターンを切り替えずに次のプレイヤーに続行させる
-          updateHighlightedBoard(); // 追加
+          alert(nextTurn === CellState.BLACK ? "黒はパスです" : "白はパスです");
+          updateHighlightedBoard();
         } else {
-          // 両者とも置けない場合 -> ゲーム終了
           setIsGameOver(true);
         }
       }
- const renderCurrentTurn = (turn: CellState) => { // 引数turnを追加
-  return `現在のターン：${turn === CellState.BLACK ? "黒" : "白"}`;
-};
-
-  return (
-    <div>
-        {/* オセロ盤の上部に現在のターンを表示する */}
-        <div className="turn-display">{renderCurrentTurn(turn)}{/* 引数turnを渡す */}</div>
-        <div className="othello-board">
-            {board.map((row, y) => row.map((_, x) => renderCell(x, y)))}
-        </div>
-    </div>
-  );
-}
-}
-
-const canPlaceStone = (x: number, y: number, turn: CellState) => {
-    if (board[y][x] !== CellState.EMPTY) {
-      return false; // 既に石が置かれている場合は置けない
     }
-  
-    const directions: [number, number][] = [
-      [0, 1], // 右
-      [0, -1], // 左
-      [1, 0], // 下
-      [-1, 0], // 上
-      [1, 1], // 右下
-      [-1, -1], // 左上
-      [1, -1], // 左下
-      [-1, 1], // 右上
-    ];
-  
-    let canPlace = false;
-  
-    for (const [dx, dy] of directions) {
-      let nx = x + dx;
-      let ny = y + dy;
-      let opponentPiecesCount = 0;
-  
-      while (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
-        if (board[ny][nx] === CellState.EMPTY) {
-          break;
-        }
-        if (board[ny][nx] !== turn) {
-          opponentPiecesCount++;
-        } else if (board[ny][nx] === turn && opponentPiecesCount > 0) {
-          canPlace = true; // 自分の石と挟んで1個以上相手の石がある場合は置ける
-          break;
-        } else {
-          break;
-        }
-        nx += dx;
-        ny += dy;
-      }
-  
-      if (canPlace) {
-        break;
-      }
-    }
-  
-    return canPlace;
   };
-
   const renderCell = (x: number, y: number) => {
     const cellState = board[y][x];
-    const isHighlighted = highlightedBoard[y][x]; // 追加
-    let cellContent;
+    const isHighlighted = highlightedBoard[y][x];
+let cellContent;
     if (cellState === CellState.BLACK) {
       cellContent = <div className="black"></div>;
     } else if (cellState === CellState.WHITE) {
@@ -229,27 +144,80 @@ const canPlaceStone = (x: number, y: number, turn: CellState) => {
     } else {
       cellContent = null;
     }
-
-    const cellClassName = isHighlighted ? 'cell highlighted' : 'cell'; // 更新
-
+    const cellClassName = isHighlighted ? "cell highlighted" : "cell";
     return (
       <div key={x} className={cellClassName} onClick={() => handleCellClick(x, y)}>
         {cellContent}
       </div>
     );
   };
-
-
+  const renderCurrentTurnAndStoneCounts = (
+    turn: CellState,
+    blackCount: number,
+    whiteCount: number
+  ) => {
+    return `現在のターン：${
+      turn === CellState.BLACK ? "黒" : "白"
+    } 黒の数：${blackCount} 白の数：${whiteCount}`;
+  };
   return (
-    <div>
-      <div className="othello-board">
-        {board.map((row, y) => row.map((_, x) => renderCell(x, y)))}
+    <div className="app-container">
+      <div className="wrapper">
+        <div className="othello-board">
+          {board.map((row, y) => row.map((_, x) => renderCell(x, y)))}
+        </div>
+        <div className="turn-display">
+          {renderCurrentTurnAndStoneCounts(
+            turn,
+            getStoneCounts(board).blackCount,
+            getStoneCounts(board).whiteCount
+          )}
+        </div>
+        <button className="reset-button" onClick={resetGame}>
+          Reset Game
+        </button>
       </div>
     </div>
   );
 };
-
-
-
-
+const canPlaceStone = (board: CellState[][], x: number, y: number, turn: CellState) => {
+  if (board[y][x] !== CellState.EMPTY) {
+    return false;
+  }
+  const directions: [number, number][] = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+    [1, 1],
+    [-1, -1],
+    [1, -1],
+    [-1, 1],
+  ];
+  let canPlace = false;
+  for (const [dx, dy] of directions) {
+    let nx = x + dx;
+    let ny = y + dy;
+    let opponentPiecesCount = 0;
+    while (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
+      if (board[ny][nx] === CellState.EMPTY) {
+        break;
+      }
+      if (board[ny][nx] !== turn) {
+        opponentPiecesCount++;
+      } else if (board[ny][nx] === turn && opponentPiecesCount > 0) {
+        canPlace = true;
+        break;
+      } else {
+        break;
+      }
+      nx += dx;
+      ny += dy;
+    }
+    if (canPlace) {
+      break;
+    }
+  }
+  return canPlace;
+};
 export default OthelloBoard;
